59294e8f490b56a36408dd82595e47d71514c404
Mateusz Kmiec <mat-kmiec@users.noreply.github.com>
2025-11-04 12:31:22 +0100

    Initial project setup

---
+++ src/test/java/pl/fishingwear/FishingWearApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fishingwear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingWearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
