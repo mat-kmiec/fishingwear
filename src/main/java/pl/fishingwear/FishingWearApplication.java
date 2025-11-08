f552db4654aa6dbd0c19509020c697315b7af5d7
Mateusz Kmiec <mat-kmiec@users.noreply.github.com>
2025-11-08 16:57:08 +0100

    feat: login functionally with first test user in liquibase

--- src/main/java/pl/fishingwear/FishingWearApplication.java
+++ src/main/java/pl/fishingwear/FishingWearApplication.java
@@ -2,12 +2,14 @@ package pl.fishingwear;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class FishingWearApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FishingWearApplication.class, args);
+        System.out.println(new BCryptPasswordEncoder().encode("test123"));
     }
 
 }
--- src/main/java/pl/fishingwear/auth/SecurityConfig.java
+++ src/main/java/pl/fishingwear/auth/SecurityConfig.java
@@ -3,6 +3,7 @@ package pl.fishingwear.auth;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,12 +12,18 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(request -> request
                         .requestMatchers("/css/**", "/js/**", "/images/**", "/libs/**").permitAll()
-                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/login", "/logowanie", "/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(login -> login
                         .loginPage("/logowanie")
                         .permitAll()
+                )
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/h2-console/**")
+                )
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
                 );
 //        TODO: Logout
         return http.build();
