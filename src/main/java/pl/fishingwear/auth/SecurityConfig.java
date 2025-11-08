53b160847df42143b8910b88b67bfc4661d1fffe
Mateusz Kmiec <mat-kmiec@users.noreply.github.com>
2025-11-08 14:12:03 +0100

    feat: create auth security config

---
+++ src/main/java/pl/fishingwear/auth/SecurityConfig.java
@@ -0,0 +1,24 @@
+package pl.fishingwear.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/css/**", "/js/**", "/images/**", "/libs/**").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .permitAll()
+                );
+        return http.build();
+    }
+}
