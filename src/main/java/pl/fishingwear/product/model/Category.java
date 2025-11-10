1d53e3d0b34ccf6ab9797231d074caedd894486a
Mateusz Kmiec <mat-kmiec@users.noreply.github.com>
2025-11-10 20:37:07 +0100

    feat: create category model

--- src/main/java/pl/fishingwear/product/model/Category.java
+++ src/main/java/pl/fishingwear/product/model/Category.java
@@ -1,4 +1,37 @@
 package pl.fishingwear.product.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "category")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String slug;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private Set<Category> children;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products;
 }
