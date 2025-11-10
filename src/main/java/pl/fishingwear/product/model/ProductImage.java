53172d49a752a953c5b7aaa0f86cb6bb62c522ac
Mateusz Kmiec <mat-kmiec@users.noreply.github.com>
2025-11-10 20:39:30 +0100

    feat: create product image model

--- src/main/java/pl/fishingwear/product/model/ProductImage.java
+++ src/main/java/pl/fishingwear/product/model/ProductImage.java
@@ -1,4 +1,32 @@
 package pl.fishingwear.product.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "product_image")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "is_main")
+    private Boolean isMain = false;
+
+    @Column(name = "sort_order")
+    private Integer sortOrder = 0;
+
 }
