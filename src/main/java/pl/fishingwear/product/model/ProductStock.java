2fa91004e9a044f6b3edfff3a59fb5807721c7df
Mateusz Kmiec <mat-kmiec@users.noreply.github.com>
2025-11-10 20:42:28 +0100

    feat: create product stock model

--- src/main/java/pl/fishingwear/product/model/ProductStock.java
+++ src/main/java/pl/fishingwear/product/model/ProductStock.java
@@ -1,4 +1,27 @@
 package pl.fishingwear.product.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "product_stock")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductStock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "size_id")
+    private Size size;
+
+    private Integer quantity = 0;
 }
