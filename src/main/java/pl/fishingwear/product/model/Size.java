68d4cef8653d2e1071675ce874afb1ec51f378c8
Mateusz Kmiec <mat-kmiec@users.noreply.github.com>
2025-11-10 20:40:32 +0100

    feat; create size model

--- src/main/java/pl/fishingwear/product/model/Size.java
+++ src/main/java/pl/fishingwear/product/model/Size.java
@@ -1,4 +1,25 @@
 package pl.fishingwear.product.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "size")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Size {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+
+
 }
