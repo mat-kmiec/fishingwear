be41ccf0a0262fb77b19099247490881fcab2f44
Mateusz Kmiec <mat-kmiec@users.noreply.github.com>
2025-11-10 20:34:49 +0100

    feat: create product model

---
+++ src/main/java/pl/fishingwear/product/model/Category.java
@@ -0,0 +1,4 @@
+package pl.fishingwear.product.model;
+
+public class Category {
+}
---
+++ src/main/java/pl/fishingwear/product/model/Product.java
@@ -0,0 +1,59 @@
+package pl.fishingwear.product.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(columnDefinition = "TEXT")
+    private String details;
+
+    private BigDecimal price;
+
+    @Column(name = "discount_price")
+    private BigDecimal discountPrice;
+
+    private String sku;
+
+    private String slug;
+
+    @Column(name = "is_active")
+    private Boolean isActive = true;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductImage> images;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductStock> stocks;
+
+    @ManyToMany
+    @JoinTable(name = "product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
+}
