1cdb923d36de193842093d88a01ba4b65681b87b
Mateusz Kmiec <mat-kmiec@users.noreply.github.com>
2025-11-10 20:45:30 +0100

    feat: create product variant model

---
+++ src/main/java/pl/fishingwear/product/model/ProductVariant.java
@@ -0,0 +1,43 @@
+package pl.fishingwear.product.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product_variant")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductVariant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "size_id")
+    private Size size;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "color_id")
+    private Color color;
+
+    @Column(name = "sku", unique = true)
+    private String sku;
+
+    private BigDecimal price;
+
+    @Column(name = "discount_price")
+    private BigDecimal discountPrice;
+
+    private Integer quantity = 0;
+
+    private Boolean isActive = true;
+}
